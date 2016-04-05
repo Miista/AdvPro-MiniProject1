2e8243dc4a6c215d4a81f578c4eeeef8f842f35f
Søren Palmund <palmund@gmail.com>
2016-04-05 14:31:06 +0200

    Added some more tests

--- src/test/scala/fpinscala/laziness/StreamSpecInauSpal.scala
+++ src/test/scala/fpinscala/laziness/StreamSpecInauSpal.scala
@@ -16,11 +16,31 @@ import Arbitrary.arbitrary
 // fail on them :)
 
 // import stream00._    // uncomment to test the book solution
-// import stream01._ // uncomment to test the broken headOption implementation
+import stream01._ // uncomment to test the broken headOption implementation
 // import stream02._ // uncomment to test another version that breaks headOption
 
 class StreamSpecInauSpal extends FlatSpec with Checkers {
   import Stream._
+
+  behavior of "headOption"
+
+  it should "not force the tail of the stream" in {
+    // val s = Stream.cons(println("hej"), Stream.cons(println("med"), Stream.cons(println("dig"), Stream.empty[Unit])))
+    // s.headOption
+    // s match {
+    //   case Empty => println("lol")
+    //   case Cons(h,t) => {
+    //     println("t:"+ isTailEval(s))
+    //   }
+    // }
+    // println("s:"+s)
+  }
+
+  def isTailEval[A](s: Stream[A]): Boolean =
+    s match {
+      case Empty => false
+      case Cons(h,t) => tailOfStream(s) == t
+    }
 
   behavior of "append"
 
@@ -67,11 +87,34 @@ class StreamSpecInauSpal extends FlatSpec with Checkers {
         s.take (n).toList == s.toList.take (n).toList } )
   }
 
+  val intStreams = genNonEmptyStream[Int]
+  val positiveInts = arbitrary[Int].suchThat (0 < _)
+
+  it should "respect idempotency" in check {
+    Prop.forAll(intStreams, positiveInts) { (s, n) => 
+      s.take(n).take(n).toList == s.take (n).toList
+    }
+  }
+
   behavior of "drop"
 
   it should "return an empty List when dropping from an empty Stream" in check {
     Prop.forAll {
       (n: Int) => empty.drop (n).toList == List.empty }
+  }
+
+  it should "respect additivity" in check {
+    implicit def arbitraryStream = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
+    implicit def intGen = Arbitrary[(Int,Int)] (
+        for {
+        n <- Gen.choose(0, Int.MaxValue/2-1)
+        m <- Gen.choose(0, Int.MaxValue/2-1)
+        } yield (n,m)
+      )
+    Prop.forAll {
+      (s: Stream[Int], v: (Int,Int)) => {
+        val (n, m) = v
+        s.drop(n).drop(m) == s.drop(n+m) } }
   }
 
   it should "drop the same elements from a List" in check {
@@ -89,13 +132,7 @@ class StreamSpecInauSpal extends FlatSpec with Checkers {
 
   Prop.forAll {
     (s: Stream[Int], n: Int) => 
-      (n < lengthOfStream (s)) ==> (lengthOfStream (s.drop (n)) == lengthOfStream (s)-n)
-  }
-  
-  // ("length of drop" |:
-  //     Prop.forAll { (s: Stream[Int], n: Int) =>
-  //       lengthOfStream (s.drop (n)) == lengthOfStream (s)-n
-  //     } )
+      (n < lengthOfStream (s)) ==> (lengthOfStream (s.drop (n)) == lengthOfStream (s)-n) }
   }
 
   // An example generator of random finite non-empty streams
@@ -116,4 +153,9 @@ class StreamSpecInauSpal extends FlatSpec with Checkers {
   def sumOfStream(s: Stream[Int]) = s.foldRight (0)(_+_)
 
   def lengthOfStream(s: Stream[Int]) = s.foldRight (0)((_,s) => s+1)
+
+  def tailOfStream[A] (s: Stream[A]) = s match {
+    case Cons(h,t) => t
+    case _ => Empty
+  }
 }
