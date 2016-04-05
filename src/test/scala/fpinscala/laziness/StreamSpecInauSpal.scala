d92809b48281a88fb58bdd880050156591afbd74
Søren Palmund <palmund@gmail.com>
2016-04-05 11:58:24 +0200

    Added first tests ("take") for Stream

--- src/test/scala/fpinscala/laziness/StreamSpecInauSpal.scala
+++ src/test/scala/fpinscala/laziness/StreamSpecInauSpal.scala
@@ -20,4 +20,37 @@ import stream00._    // uncomment to test the book solution
 // import stream02._ // uncomment to test another version that breaks headOption
 
 class StreamSpecInauSpal extends FlatSpec with Checkers {
+  import Stream._
+
+  behavior of "take"
+
+  it should "return an empty List when taking from an empty Stream" in {
+    assert (empty.take (5).toList == List.empty)
+  }
+
+  it should "return the 10 first elements of an arbitrary Stream" in check {
+    implicit def arbitraryIntList = Arbitrary[List[Int]] (genNonEmptyList[Int])
+    implicit def intGen = Arbitrary[Int] (Gen.choose(0,200))
+
+    ("take from stream" |: 
+      Prop.forAll { (l: List[Int], n: Int) =>
+        list2stream (l).take (n).toList == l.take (n) } ) && 
+    ("take then stream" |:
+      Prop.forAll { 
+        (la: List[Int], n: Int) => list2stream (la).take (n).toList == list2stream (la.take (n)).toList } )
+  }
+
+  // An example generator of random finite non-empty streams
+  def list2stream[A] (la :List[A]): Stream[A] = la.foldRight (empty[A]) (cons[A](_,_))
+
+  // In ScalaTest we use the check method to switch to ScalaCheck's internal DSL
+  def genNonEmptyStream[A] (implicit arbA :Arbitrary[A]) :Gen[Stream[A]] =
+    for {
+      la <- arbitrary[List[A]] suchThat (_.nonEmpty)
+    } yield list2stream (la)
+
+  def genNonEmptyList[A] (implicit arbA :Arbitrary[A]) :Gen[List[A]] =
+    for {
+      la <- arbitrary[List[A]] suchThat (_.nonEmpty)
+    } yield la
 }
