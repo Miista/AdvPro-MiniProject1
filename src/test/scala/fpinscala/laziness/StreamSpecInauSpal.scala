d32bb1c86b75b8cd27a364049bd356f0a3731de4
Søren Palmund <palmund@gmail.com>
2016-04-05 11:15:35 +0200

    Create InauSpal spec file

---
+++ src/test/scala/fpinscala/laziness/StreamSpecInauSpal.scala
@@ -0,0 +1,23 @@
+// Advanced Programming
+// Andrzej Wasowski, IT University of Copenhagen
+
+package fpinscala.laziness
+import scala.language.higherKinds
+
+import org.scalatest.FlatSpec
+import org.scalatest.prop.Checkers
+import org.scalacheck._
+import org.scalacheck.Prop._
+import Arbitrary.arbitrary
+
+// If you comment out all the import lines below, then you test the Scala
+// Standard Library implementation of Streams. Interestingly, the standard
+// library streams are stricter than those from the book, so some laziness tests
+// fail on them :)
+
+import stream00._    // uncomment to test the book solution
+// import stream01._ // uncomment to test the broken headOption implementation
+// import stream02._ // uncomment to test another version that breaks headOption
+
+class StreamSpecInauSpal extends FlatSpec with Checkers {
+}
